9f8305bb4ab67fca921a0f00ccbaa986f8152442
edito13 <editotchocosso13@gmail.com>
2026-01-06 09:04:22 +0100

    create progress component to know how is the current state of the target

---
+++ src/components/Progress/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+interface SavedValue {
+  current: string;
+  target: string;
+  percentage: number;
+}
+
+interface ProgressProps {
+  data: SavedValue;
+}
+
+const Progress: React.FC<ProgressProps> = ({ data }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Valor guardado</Text>
+      <View style={styles.status}>
+        <Text style={styles.value}>
+          {data.current}
+          <Text style={styles.target}> de {data.target}</Text>
+        </Text>
+        <Text style={styles.percentage}>{data.percentage.toFixed(0)}%</Text>
+      </View>
+      <View style={styles.progress}>
+        <View
+          style={[styles.currentProgress, { width: `${data.percentage}%` }]}
+        ></View>
+      </View>
+    </View>
+  );
+};
+
+export default Progress;
---
+++ src/components/Progress/styles.ts
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native";
+import { colors, fontFamily } from "@/theme";
+
+export const styles = StyleSheet.create({
+  container: { width: "100%" },
+  label: {
+    fontSize: 12,
+    color: colors.gray[500],
+    fontFamily: fontFamily.medium,
+    marginBottom: 5,
+  },
+  status: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "flex-end",
+  },
+  value: {
+    flex: 1,
+    fontSize: 18,
+    color: colors.black,
+    fontFamily: fontFamily.medium,
+  },
+  target: {
+    fontSize: 14,
+    fontFamily: fontFamily.medium,
+    color: colors.gray[500],
+  },
+
+  percentage: {
+    fontSize: 14,
+    fontFamily: fontFamily.bold,
+    color: colors.blue[500],
+  },
+  progress: {
+    marginTop: 16,
+    width: "100%",
+    height: 5,
+    borderRadius: 5,
+    backgroundColor: colors.gray[300],
+    overflow: "hidden",
+  },
+  currentProgress: {
+    height: 5,
+    backgroundColor: colors.blue[500],
+  },
+});
