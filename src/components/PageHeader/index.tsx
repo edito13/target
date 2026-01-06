d0864a33659e433cbdabb8731f880541b6d011cd
edito13 <editotchocosso13@gmail.com>
2026-01-06 09:06:36 +0100

    create PageHeader component

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+import { MaterialIcons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { colors } from "@/theme";
+
+interface PageHeaderProps {
+  title: string;
+  subtitle?: string;
+  rightButton?: {
+    onPress: () => void;
+    icon: keyof typeof MaterialIcons.glyphMap;
+  };
+}
+
+const PageHeader: React.FC<PageHeaderProps> = ({
+  title,
+  subtitle,
+  rightButton,
+}) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity activeOpacity={0.8} onPress={() => router.back()}>
+          <MaterialIcons name="arrow-back" size={32} color={colors.black} />
+        </TouchableOpacity>
+        {rightButton && (
+          <TouchableOpacity onPress={rightButton.onPress}>
+            <MaterialIcons
+              name={rightButton.icon}
+              size={24}
+              color={colors.gray[500]}
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+      <Text style={styles.title}>{title}</Text>
+      {subtitle && <Text style={styles.subtitle}>{subtitle}</Text>}
+    </View>
+  );
+};
+
+export default PageHeader;
